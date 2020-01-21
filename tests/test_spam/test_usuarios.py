192e963595b379505c466e9dc820880a476255d3
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-21 17:59:53 -0300

    created test_usuarios

---
+++ tests/test_spam/test_usuarios.py
@@ -0,0 +1,21 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Ernani')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_listar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Ernani', Usuario='Rose')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuario == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
