e38f7eb0c19d444def78a9bf9d34453806764380
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-21 20:33:49 -0300

    update file

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,7 +2,7 @@ from main import EnviadorDeSpam
 from spam.enviador_de_email import Enviador
 
 
-def test_envio_de_spam():
+def test_envio_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
         'ernanidacosta@gmail.com',
