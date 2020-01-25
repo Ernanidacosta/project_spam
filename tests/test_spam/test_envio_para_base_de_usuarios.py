c211c51ba776cfc57a46c71d2f5193626e8dfe74
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-25 14:52:23 -0300

    implemented pytest parametrize

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from main import EnviadorDeSpam
+from modelos import Usuario
 from spam.enviador_de_email import Enviador
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Ernani', email='ernanidacosta@gmail.com'),
+            Usuario(nome='Rose', email='ernanidacosta@gmail.com')
+        ],
+        [
+            Usuario(nome='Ernani', email='ernanidacosta@gmail.com'),
+        ]
+    ]
+)
+def test_quantidade_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+        enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'ernanidacosta@gmail.com',
         'Teste envio de spam do PythonPro',
         'Se recebeu, fuunciona!!'
     )
+    assert len(usuarios) == enviador.quantidade_email_enviado
