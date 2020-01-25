a50b3aae93ce22de98584c2a484c45058863c659
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-25 14:51:57 -0300

    implemented method quantidade_email_enviado

--- spam/enviador_de_email.py
+++ spam/enviador_de_email.py
@@ -1,7 +1,11 @@
 class Enviador:
+    def __init__(self):
+        self.quantidade_email_enviado = 0
+
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email do remetente inválido: {remetente}')
+        self.quantidade_email_enviado += 1
         return remetente
 
 
