46574674379e8f8c17c114e6e5cede218261a653
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-25 14:52:35 -0300

    update file

--- tests/test_spam/test_usuarios.py
+++ tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Ernani')
+    usuario = Usuario(nome='Ernani', email='ernanidacosta@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Ernani'), Usuario(nome='Rose')]
+    usuarios = [Usuario(nome='Ernani', email='ernanidacosta@gmail.com'),
+                Usuario(nome='Rose', email='ernanidacosta@gmail.com')
+                ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
