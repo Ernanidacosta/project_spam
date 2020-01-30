93449a7233db2b0da52c1734a317c1e46490d45b
Ernani da Costa <ernanidacosta@gmail.com>
2020-01-30 16:27:57 -0300

    Implement Mocker from Pytest

--- tests/test_spam/test_github_api.py
+++ tests/test_spam/test_github_api.py
@@ -4,18 +4,21 @@ import pytest
 
 from spam import github_api
 
+
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars3.githubusercontent.com/u/11460643?v=4'
     resp_mock.json.return_value = {
         "login": "Ernanidacosta", "id": 11460643,
         "avatar_url": url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('spam.github_api.requests.get')
+    #   get_original = github_api.requests.get
+    get_mock.return_value=resp_mock
+    return url
+
+    #    github_api.requests.get = get_original
 
 
 def test_buscar_avatar(avatar_url):
